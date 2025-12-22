1b974d5c1b75968caa8d6043cbfeb6a258979a00
Ilia Baev <baevilia.201@yandex.ru>
2025-12-22 17:44:59 +0300

    Добавил Swagger

---
+++ RequestsService/Controllers/RequestsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RequestsService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RequestsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetRequests(int id)
+    {
+        return Ok();
+    }
+}

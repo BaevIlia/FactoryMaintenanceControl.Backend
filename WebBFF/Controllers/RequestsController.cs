8aa7f5bbf478265ab427d2beb927efcd480bfa5a
Ilia Baev <baevilia.201@yandex.ru>
2026-01-17 21:22:06 +0300

    Добавил Swagger

---
+++ WebBFF/Controllers/RequestsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBFF.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RequestsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetRequests()
+    {
+        return Ok();
+    }
+}

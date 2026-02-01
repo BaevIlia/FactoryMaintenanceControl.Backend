fc5eee6d14d64175b222fc958e35015a1fd22490
Ilia Baev <baevilia.201@yandex.ru>
2026-02-01 17:37:59 +0300

    Создал проект, добавил Swagger

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Ping()
+    {
+        return Ok("Good");
+    }
+}
